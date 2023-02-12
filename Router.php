dd9112207ccad4312419016f6efd0c4f4e69d04a
ahmedali-dev <104825374+ahmedali-dev@users.noreply.github.com>
2023-02-12 09:45:13 +0200

    Add files via upload

---
+++ Router.php
@@ -0,0 +1,169 @@
+<?php
+
+
+class Route
+{
+
+
+    /**
+     * @var array $routes
+     */
+
+    public static array $routes = array();
+
+
+    /**
+     * @var string $NotFoundClass
+     */
+    public static string $NotFoundClass = 'notFound';
+
+    /**
+     * @var string $NotFoundFun
+     * */
+    public static string $NotFoundFun = 'index';
+
+
+
+    /**
+     * ! @param string $path => url path http://site.com/home
+     * ! @param callable|string $con => class required
+     */
+
+    public static function get(string $path, callable|string $con): void
+    {
+        self::AddRoute('get', $path, $con);
+    }
+
+    /**
+     * ! @param string $path => url path http://site.com/home
+     * ! @param callable|string $con => class required
+     */
+
+    public static function post(string $path, callable|string $con): void
+    {
+        self::AddRoute('post', $path, $con);
+    }
+
+
+    /**
+     * ! @param string $path => url path http://site.com/home
+     * ! @param callable|string $con => class required
+     */
+
+    public static function delete(string $path, callable|string $con): void
+    {
+        self::AddRoute('delete', $path, $con);
+    }
+
+
+    /**
+     * ! @param string $path => url path http://site.com/home
+     * ! @param callable|string $con => class required
+     */
+
+    public static function put(string $path, callable|string $con): void
+    {
+        self::AddRoute('put', $path, $con);
+    }
+
+    /**
+     * @param string $method
+     * @param string $path
+     * @param string|callable $con => controller
+     */
+
+    private static function AddRoute(string $method, string $path, string|callable $con): void
+    {
+        self::$routes[strtolower($path)] = array(
+            'method' => $method,
+            'controller' => $con
+        );
+    }
+
+    public static function Dispatch(): void
+    {
+
+        $url_parts = explode("/", substr(strtolower(self::getRequestPath()), 1));
+        foreach (self::$routes as $route => $value) {
+            $params = [];
+//            echo $route . "<br>";
+            $path_parts = explode('/', substr($route, 1));
+
+            if (count($path_parts) !== count($url_parts)) {
+                continue;
+            }
+
+            if (self::GetMethod() !== $value['method']) {
+                continue;
+            }
+
+            foreach ($path_parts as $idx => $val) {
+
+                if (strpos($val, '{') === 0) {
+                    $params[substr($val, 1, -1)] = $url_parts[$idx];
+                }else if ($val !== $url_parts[$idx]) {
+                    continue 2;
+                }
+
+            }
+
+//            echo $route;
+//            echo "<br>".$value['controller'];
+
+            if (is_callable($value['controller'])) {
+                $value['controller']($params);
+            }else {
+                $controller = explode('@', $value['controller']);
+                $controller_class = $controller[0];
+                $controller_fun = $controller[1];
+                call_user_func_array(array(new $controller_class, $controller_fun), array(...array_values($params)));
+            }
+            return;
+        }
+
+        self::ErrorPage();
+
+    }
+
+    public static function ErrorPage()
+    {
+        return call_user_func_array(array(new self::$NotFoundClass, self::$NotFoundFun), array());
+    }
+
+
+    /**
+     * get url request and handler
+     * @return string
+     *
+     */
+
+    public static function getRequestPath(): string
+    {
+        $path = $_SERVER['REQUEST_URI'];
+        $pos = strpos($path, '?');
+        if (!$pos) {
+            return $path;
+        } else {
+            return substr($path, 0, $pos);
+        }
+    }
+
+    /**
+     * Get request method
+     * @return string
+     */
+
+    public static function GetMethod(): string
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
+
+
+    public static function print($r): void
+    {
+        echo "<pre>";
+        var_dump($r);
+        echo "</pre>";
+    }
+}
